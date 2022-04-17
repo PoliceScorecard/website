dd932198f9288b756a5d487f923d26c7f94cf876
Peter Schmalfeldt <peter@manifestinteractive.com>
2022-04-17 18:27:12 -0400

    Fix incorrect year

--- app/Helpers.php
+++ app/Helpers.php
@@ -914,9 +914,9 @@ if (!function_exists('getChange')) {
                 $end = '2023';
             }
 
-            // TODO: This is a temp fix to resolve an issue with incorrect labels, need to fix this on the API side
+            // TODO: This is a temp fix to use 2016-2020 to resolve an issue with incorrect labels, need to fix this on the API side
             $start = '2016';
-            $end = '2021';
+            $end = '2020';
 
             if ($start && $end && $start !== $end) {
                 $label = "from {$start}-" . substr($end, -2);
